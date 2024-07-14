833be8541e51e7846b5ad387f09d3264c3c02d67
leaf-lxh <meleaf@qq.com>
2024-07-14 23:11:49 +0800

    feat: improved interactive experience， add date distance calculating

--- src/components/VideoCard/VideoCard.tsx
+++ src/components/VideoCard/VideoCard.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import SlideshowOutlinedIcon from '@mui/icons-material/SlideshowOutlined';
 import ArticleOutlinedIcon from '@mui/icons-material/ArticleOutlined';
 import { Box, styled, Typography } from '@mui/material';
+import { formatDistance } from 'date-fns';
+import { zhCN } from "date-fns/locale";
 
 interface VideoCardProps {
   video: {
@@ -47,8 +49,11 @@ const VideoCard: React.FC<VideoCardProps> = ({ video, width, height }) => {
         width: width || 'auto',
         height: height || 'auto'
       }}>
-      <Box sx={{ position: 'relative', borderRadius: 1, overflow: 'hidden', width: '100%' }}>
-        <img src={cover_img} alt={title} style={{ display: 'block', width: '100%', height: 'auto' }} />
+      {/* 封面部分 */}
+      <Box component='a' href="/video/1" target='_blank' sx={{ position: 'relative', borderRadius: 1, overflow: 'hidden', width: '100%', ":hover": { cursor: 'pointer', ".video-card-video-stat": { color: "#98c3e6" } } }}>
+        {/* 封面 */}
+        <img src={cover_img} alt={title} style={{ display: 'block', width: '100%', height: 'auto', aspectRatio: '16 / 9' }} />
+        {/* 视频数据 */}
         <Box
           sx={{
             display: 'flex',
@@ -56,22 +61,24 @@ const VideoCard: React.FC<VideoCardProps> = ({ video, width, height }) => {
             bottom: 0,
             left: 0,
             width: '100%',
-            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            backgroundColor: 'rgba(0, 0, 0, 0.6)',
             color: 'white',
-            padding: 1,
+            padding: '4px 6px',
             boxSizing: 'border-box',
+            borderRadius: '4px'
           }}
         >
-          <SlideshowOutlinedIcon sx={{ fontSize: 16 }} />
-          <Typography sx={{ margin: '0 6px 0 2px' }} variant='caption'>{watched}</Typography>
-          <ArticleOutlinedIcon sx={{ fontSize: 16 }} />
-          <Typography sx={{ margin: '0 6px 0 2px' }} variant='caption'>{danmaku_nums}</Typography>
-          <Typography sx={{ marginLeft: 'auto', marginRight: 0 }} variant='caption'>{formatDuration(duration)}</Typography>
+          <SlideshowOutlinedIcon className="video-card-video-stat" sx={{ fontSize: 16 }} />
+          <Typography className="video-card-video-stat" sx={{ margin: '0 6px 0 2px' }} variant='caption'>{watched}</Typography>
+          <ArticleOutlinedIcon className="video-card-video-stat" sx={{ fontSize: 16 }} />
+          <Typography className="video-card-video-stat" sx={{ margin: '0 6px 0 2px' }} variant='caption'>{danmaku_nums}</Typography>
+          <Typography className="video-card-video-stat" sx={{ marginLeft: 'auto', marginRight: 0 }} variant='caption'>{formatDuration(duration)}</Typography>
         </Box>
       </Box>
+      {/* 视频标题 */}
       <Typography
         sx={{
-          padding: '2px 0px', 
+          padding: '2px 0px',
           marginTop: '6px',
           overflow: 'hidden',
           textOverflow: 'ellipsis',
@@ -81,14 +88,16 @@ const VideoCard: React.FC<VideoCardProps> = ({ video, width, height }) => {
           fontSize: '0.95em',
           lineHeight: '1.5em',
           height: '3em',
+          ":hover": { cursor: 'pointer', color: "#6faee0"}
         }}
       >
         {title}
       </Typography>
-      <Box sx={{ color: 'rgb(129, 129, 129)', display: 'flex' }}>
-        <Typography variant='subtitle2'>{`${uploader} · 6-30`}</Typography>
+      {/* 视频上传者与时间信息 */}
+      <Box sx={{ color: 'rgb(129, 129, 129)', display: 'flex', ":hover": { cursor: 'pointer', color: "#6faee0" } }}>
+        <Typography variant='subtitle2'>{`${uploader} · ${formatDistance(video.date, new Date(), { locale: zhCN, addSuffix: true })}`}</Typography>
       </Box>
-    </Box>
+    </Box >
   );
 };
 
