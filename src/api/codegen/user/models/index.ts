77ef418a259a8ff44d3fc6c3bb7f0f61050df501
leaf-lxh <meleaf@qq.com>
2024-07-14 18:26:28 +0800

    feat: add user service api codegen

---
+++ src/api/codegen/user/models/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './DailyBonusVO';
+export * from './EmailLoginDTO';
+export * from './ResponseDailyBonusVO';
+export * from './ResponseObject';
+export * from './ResponseUserDetailVO';
+export * from './SendLoginEmailCodeDTO';
+export * from './UpdateUserDetailDTO';
+export * from './UserDetailVO';
