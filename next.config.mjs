9dedf26c92339283da4bddfaf960b26a29d5fd68
leaf-lxh <meleaf@qq.com>
2024-09-04 13:46:17 +0800

    feat: 添加本地开发环境到API服务器的转发

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,32 @@
 /** @type {import('next').NextConfig} */
+import { env } from 'process';
+
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: async function () {
+    if (env.NODE_ENV === 'development') {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://localhost/api/:path*',
+        },
+        {
+          source: '/swagger/:path*',
+          destination: 'http://localhost/swagger/:path*',
+        },
+        {
+          source: '/v3/:path*',
+          destination: 'http://localhost/v3/:path*',
+        },
+        {
+          source: '/webjars/:path*',
+          destination: 'http://localhost/webjars/:path*',
+        },
+      ]
+    }
+
+    return [];
+  },
 };
 
 export default nextConfig;
