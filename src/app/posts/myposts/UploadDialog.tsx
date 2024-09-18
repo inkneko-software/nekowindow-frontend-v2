a76a12c645ec429a4c062b5d93bdb10c147f0ada
leaf-lxh <meleaf@qq.com>
2024-09-18 23:39:38 +0800

    feat: 实现视频上传

--- src/app/posts/myposts/UploadDialog.tsx
+++ src/app/posts/myposts/UploadDialog.tsx
@@ -22,8 +22,10 @@ import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import AddPhotoAlternateOutlinedIcon from '@mui/icons-material/AddPhotoAlternateOutlined';
 import theme from '@theme/theme';
-import { Chip, Stack, Tab, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tabs } from '@mui/material';
+import { Chip, FormHelperText, Stack, Tab, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tabs } from '@mui/material';
 import { Configuration, PartitionInfo, VideoControllerApi } from '@api/codegen/video';
+import { SnackbarProvider, enqueueSnackbar } from 'notistack';
+import { useRouter } from 'next/navigation'
 
 const videoapi = new VideoControllerApi(new Configuration({ credentials: 'include' }));
 interface UploadDialogProps {
@@ -38,22 +40,36 @@ export const UploadDialog: React.FC<UploadDialogProps> = ({ open, onClose }) => 
     '公开范围',
   ];
   const [currentStep, setCurrentStep] = React.useState<number>(0);
-  // 是否已选择视频文件
+  //是否已选择视频文件
   const [isVideoSelected, setIsVideoSelected] = React.useState(false);
-  // 视频预览链接
+  //视频预览链接
   const [videoPreviewUrl, setVideoPreviewUrl] = React.useState('');
-  // 封面预览链接
+  //封面预览链接
   const [coverPreviewUrl, setCoverPreviewUrl] = React.useState('');
-  // 视频上传预签名链接
+  //视频上传预签名链接
   const [videoUploadUrl, setVideoUploadUrl] = React.useState('');
-  // 分区
+  //视频标题
+  const [videoTitle, setVideoTitle] = React.useState('');
+  //视频描述
+  const [videoDescription, setVideoDescription] = React.useState('');
+  //分区
   const [partitions, setPartitions] = React.useState<PartitionInfo[]>([]);
-  // 已选择的分区
-  const [selectedPartition, setSelectedPartition] = React.useState();
-  // 当前分区推荐的标签
+  //已选择的分区
+  const [selectedPartition, setSelectedPartition] = React.useState(0);
+  //当前分区推荐的标签
   const [recommendTags, setRecommendTags] = React.useState<string[]>([]);
-  // 已选择的标签
+  //已选择的标签
   const [tags, setTags] = React.useState<string[]>([]);
+  //用户输入的标签
+  const [inputTag, setInputTag] = React.useState('')
+
+  const [errorHints, setErrorHints] = React.useState({
+    title: '',
+    video: '',
+    partition: '',
+  })
+
+  const router = useRouter();
 
   React.useEffect(() => {
     videoapi.getPartitionList().then(res => {
@@ -64,6 +80,8 @@ export const UploadDialog: React.FC<UploadDialogProps> = ({ open, onClose }) => 
   }, [])
 
   const handleSelectVideoFile = async () => {
+    //清除错误提示
+    setErrorHints({ ...errorHints, video: '' });
     try {
       var res1 = await videoapi.generateUploadUrl();
       if (res1.code !== 0) {
@@ -164,6 +182,7 @@ return new Promise<ResponseMusic>((resolve, reject) => {
    * 分区更换选择的处理逻辑
    */
   const handlePartitionChange = (partitionId: number) => {
+    setSelectedPartition(partitionId);
     videoapi.getPartitionRecommendTagList({ partitionId: partitionId }).then(res => {
       if (res.data !== undefined) {
         setRecommendTags(res.data)
@@ -215,12 +234,70 @@ return new Promise<ResponseMusic>((resolve, reject) => {
     })
   }
 
-  const handleCreatePost = () =>{
+  /**
+   * 提交稿件信息
+   */
+  const handleCreatePost = () => {
+    videoapi.createVideoPost({
+      createVideoPostDto: {
+        title: videoTitle,
+        description: videoDescription,
+        coverUrl: coverPreviewUrl,
+        videoUrl: videoPreviewUrl,
+        partitionId: selectedPartition,
+        tags: tags
+      }
+    }).then(res => {
+      if (res.code !== 0) {
+        enqueueSnackbar(res.message, { variant: 'error' })
+        return;
+      }
+
+      enqueueSnackbar(res.message);
+      setTimeout(() => {
+        router.replace(`/video/${res.data?.nkid}`)
+      }, 1000)
+    })
+  }
+
+  /**
+   * 跳转至下一个步骤
+   */
+  const handleNextStep = (targetStep: number) => {
+    if (targetStep === currentStep) {
+      return;
+    }
+
+    //稿件信息检查
+    if (currentStep === 0) {
+      var passed = true;
+      //检查视频标题是否为空
+      if (videoTitle === '') {
+        setErrorHints(prev => ({ ...prev, title: '请填写视频标题' }))
+        passed = false;
+      }
+
+      if (selectedPartition === 0) {
+        setErrorHints(prev => ({ ...prev, partition: '请选择分区' }))
+        passed = false;
+      }
+
+      if (videoPreviewUrl === '') {
+        setErrorHints(prev => ({ ...prev, video: '请在上传完成后进行提交' }))
+        passed = false;
+      }
+
+      if (!passed) {
+        return;
+      }
+    }
+
+    setCurrentStep(currentStep + 1);
 
   }
 
   return (
-    <Dialog open={open} onClose={onClose} sx={{ display: 'flex', flexDirection: 'column', ".MuiPaper-root": { width: '960px', borderRadius: '18px', height: 'calc(100vh - 128px)' } }} maxWidth={false}>
+    <Dialog open={open} sx={{ display: 'flex', flexDirection: 'column', ".MuiPaper-root": { width: '960px', borderRadius: '18px', height: 'calc(100vh - 128px)' } }} maxWidth={false}>
       {/* 视频文件上传控件 */}
       <input type='file' hidden id='video-upload-input' />
       {/* 封面文件的上传控件 */}
@@ -236,7 +313,7 @@ return new Promise<ResponseMusic>((resolve, reject) => {
       {/* 稿件信息填写进度 */}
       <Stepper activeStep={currentStep} alternativeLabel>
         {steps.map((label, index) => (
-          <Step key={label} onClick={() => setCurrentStep(index)}  >
+          <Step key={label} onClick={() => handleNextStep(index)}  >
             <StepLabel sx={{ cursor: 'pointer' }} >{label}</StepLabel>
           </Step>
         ))}
@@ -249,9 +326,31 @@ return new Promise<ResponseMusic>((resolve, reject) => {
           <Box sx={{ display: 'flex', width: '66%', flexDirection: 'column' }}>
             <Typography variant='h5' fontWeight='bold' sx={{ marginBottom: 1 }}>视频信息</Typography>
             {/* 视频标题 */}
-            <TextField sx={{ width: '100%', margin: '16px 0px' }} label="视频标题（必填）" InputLabelProps={{ shrink: true }} placeholder='添加一个可以描述你视频的标题' variant='outlined' autoFocus></TextField>
+            <TextField
+              sx={{ width: '100%', margin: '16px 0px' }}
+              label="视频标题（必填）"
+              InputLabelProps={{ shrink: true }}
+              placeholder='添加一个可以描述你视频的标题'
+              variant='outlined'
+              autoFocus
+              value={videoTitle}
+              onChange={e => setVideoTitle(e.target.value)}
+              onFocus={() => setErrorHints({ ...errorHints, title: '' })}
+              error={errorHints.title !== ''}
+              helperText={errorHints.title}
+            />
             {/* 视频描述 */}
-            <TextField sx={{ width: '100%', margin: '16px 0px' }} label="说明" InputLabelProps={{ shrink: true }} placeholder='向观看者介绍你的视频' multiline minRows={3} variant='outlined'></TextField>
+            <TextField
+              sx={{ width: '100%', margin: '16px 0px' }}
+              label="说明"
+              InputLabelProps={{ shrink: true }}
+              placeholder='向观看者介绍你的视频'
+              multiline
+              minRows={3}
+              variant='outlined'
+              value={videoDescription}
+              onChange={e => setVideoDescription(e.target.value)}
+            />
             {/* 封面 */}
             <Typography variant='subtitle1'>封面</Typography>
             <Typography variant='subtitle2' sx={{ color: theme.palette.text.secondary }}>设置与众不同的缩略图，吸引观看者注意。</Typography>
@@ -278,43 +377,56 @@ return new Promise<ResponseMusic>((resolve, reject) => {
                 }
                 {
                   coverPreviewUrl !== '' &&
-                  <Box component='img' src={coverPreviewUrl} sx={{width: '100%', height: '100%'}}/>
+                  <Box component='img' src={coverPreviewUrl} sx={{ width: '100%', height: '100%' }} />
                 }
               </Box>
             </Box>
             {/* 分区 */}
             <Typography variant='subtitle1' sx={{ marginTop: '8px' }}>分区</Typography>
             <Typography variant='subtitle2' sx={{ color: theme.palette.text.secondary }}>选择视频相对应的分区，物以类聚</Typography>
-            <Select
-              sx={{ width: 'fit-content', minWidth: '128px' }}
-              onChange={e => handlePartitionChange(e.target.value as number)}
-              defaultValue={0}
-            >
-              <MenuItem value={0}>请选择分区</MenuItem>
-              {
-                partitions.map((partition, index) => {
-                  return <MenuItem value={partition.partitionId} key={index}>{partition.partitionName}</MenuItem>
-                })
-              }
-            </Select>
+            <FormControl error={errorHints.partition !== ''}>
+              <Select
+                sx={{ width: 'fit-content', minWidth: '128px' }}
+                onChange={e => handlePartitionChange(e.target.value as number)}
+                defaultValue={0}
+                onFocus={() => setErrorHints({ ...errorHints, partition: '' })}
+              >
+                <MenuItem value={0}>请选择分区</MenuItem>
+                {
+                  partitions.map((partition, index) => {
+                    return <MenuItem value={partition.partitionId} key={index}>{partition.partitionName}</MenuItem>
+                  })
+                }
+              </Select>
+              <FormHelperText>{errorHints.partition}</FormHelperText>
+            </FormControl>
+
             {/* 标签 */}
             <Typography variant='subtitle1' sx={{ marginTop: '8px' }}>标签</Typography>
             <Typography variant='subtitle2' sx={{ color: theme.palette.text.secondary }}>视频内容的标签</Typography>
             {/* 已选择标签 */}
-            <Stack direction='row' spacing={1} sx={{ marginTop: '8px' }}>
+            <Box sx={{ marginTop: '8px', display: 'flex', flexWrap: 'wrap' }}>
               {
                 tags.map((tag, index) => {
-                  return <Chip label={tag} onDelete={() => handleDeleteTag(tag)} />
+                  return <Chip sx={{ margin: '4px 4px 4px 0px' }} label={tag} onDelete={() => handleDeleteTag(tag)} />
                 })
               }
-            </Stack>
+            </Box>
             {/* 标签输入框 */}
             <TextField
               sx={{ marginTop: '8px' }}
               placeholder='回车或点击按钮添加标签'
               InputProps={{
-                endAdornment: <Button>添加</Button>,
+                endAdornment: <Button onClick={() => { handleAddTag(inputTag); setInputTag('') }}>添加</Button>,
               }}
+              onChange={e => setInputTag(e.target.value)}
+              onKeyUp={e => {
+                if (e.key === 'Enter') {
+                  handleAddTag(inputTag);
+                  setInputTag('')
+                }
+              }}
+              value={inputTag}
             />
             {/* 推荐标签 */}
             {
@@ -322,14 +434,14 @@ return new Promise<ResponseMusic>((resolve, reject) => {
               <Typography variant='subtitle2' sx={{ color: theme.palette.text.secondary, marginTop: '8px' }}>分区推荐标签：</Typography>
 
             }
-            <Stack direction='row' spacing={1} sx={{ marginTop: '8px' }}>
+            <Box sx={{ marginTop: '8px', display: 'flex', flexWrap: 'wrap' }}>
               {
                 recommendTags.map((tag, index) => {
 
-                  return <Chip label={tag} onClick={() => handleAddTag(tag)} />
+                  return <Chip sx={{ margin: '4px 4px 4px 0px' }} label={tag} onClick={() => handleAddTag(tag)} />
                 })
               }
-            </Stack>
+            </Box>
             {/* 评论控制 */}
             <Typography variant='subtitle1' sx={{ marginTop: '8px' }}>评论</Typography>
             <Typography variant='subtitle2' sx={{ color: theme.palette.text.secondary }}>评论控制</Typography>
@@ -337,12 +449,12 @@ return new Promise<ResponseMusic>((resolve, reject) => {
             <FormControl>
               <RadioGroup
                 aria-labelledby="demo-radio-buttons-group-label"
-                defaultValue="female"
+                defaultValue="enabled"
                 name="radio-buttons-group"
                 row
               >
-                <FormControlLabel value="female" control={<Radio />} label="开启" />
-                <FormControlLabel value="male" control={<Radio />} label="关闭" />
+                <FormControlLabel value="enabled" control={<Radio />} label="开启" />
+                <FormControlLabel value="disabled" control={<Radio />} label="关闭" />
               </RadioGroup>
             </FormControl>
             {/* 视频水印 */}
@@ -351,7 +463,7 @@ return new Promise<ResponseMusic>((resolve, reject) => {
             <FormControl>
               <RadioGroup
                 aria-labelledby="demo-radio-buttons-group-label"
-                defaultValue="female"
+                defaultValue="disabled"
                 name="radio-buttons-group"
                 row
               >
@@ -359,7 +471,7 @@ return new Promise<ResponseMusic>((resolve, reject) => {
                 <FormControlLabel value="left_down" control={<Radio />} label="左下" />
                 <FormControlLabel value="right_up" control={<Radio />} label="右上" />
                 <FormControlLabel value="right_down" control={<Radio />} label="右下" />
-                <FormControlLabel value="male" control={<Radio />} label="关闭" />
+                <FormControlLabel value="disabled" control={<Radio />} label="关闭" />
               </RadioGroup>
             </FormControl>
           </Box>
@@ -375,7 +487,7 @@ return new Promise<ResponseMusic>((resolve, reject) => {
             {
               videoPreviewUrl !== '' &&
               <>
-                <Box sx={{ height: '172px', margin: 1, borderRadius: 2 }} component='video' src={videoPreviewUrl}></Box>
+                <Box sx={{ margin: 1, borderRadius: 2 }} component='video' src={videoPreviewUrl} controls></Box>
                 <Typography variant='caption' sx={{ margin: 1 }}>视频链接</Typography>
                 <Typography sx={{ margin: 1 }} noWrap>{videoPreviewUrl}</Typography>
               </>
@@ -388,7 +500,10 @@ return new Promise<ResponseMusic>((resolve, reject) => {
                 <Button onClick={handleReupload}>重新上传</Button>
               </>
             }
-
+            {
+              errorHints.video !== '' &&
+              <FormHelperText error sx={{ margin: '8px auto' }}>{errorHints.video}</FormHelperText>
+            }
           </Box>
         </Box>
       }
@@ -421,7 +536,7 @@ return new Promise<ResponseMusic>((resolve, reject) => {
         }
         {
           currentStep != 2 &&
-          <Button variant='contained' sx={{ margin: 'auto 8px', borderRadius: 8 }} onClick={() => setCurrentStep(prev => prev + 1)}> 继续</Button>
+          <Button variant='contained' sx={{ margin: 'auto 8px', borderRadius: 8 }} onClick={() => handleNextStep(currentStep + 1)}> 继续</Button>
         }
       </Box>
     </Dialog >
