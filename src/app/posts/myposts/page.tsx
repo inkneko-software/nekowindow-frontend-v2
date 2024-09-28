ddd94d2fac86a75c2fcc0933352abd204e007277
leaf-lxh <meleaf@qq.com>
2024-09-28 14:03:21 +0800

    feat: 对接后端稿件查询API

--- src/app/posts/myposts/page.tsx
+++ src/app/posts/myposts/page.tsx
@@ -25,10 +25,23 @@ import theme from '@theme/theme';
 import { Tab, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tabs } from '@mui/material';
 import { UploadDialog } from './UploadDialog';
 
+import { Configuration, PartitionInfo, UserUploadedVideoStatisticsVO, VideoControllerApi, VideoManagementControllerApi, VideoPostBriefVO } from '@api/codegen/video';
+
+var videoAPI = new VideoControllerApi(new Configuration({ credentials: "include" }))
+
+var videoManagementAPI = new VideoManagementControllerApi(new Configuration({ credentials: "include" }))
 
 const UploadHome: React.FC = () => {
   const [uploadDialogOpen, setUploadDialogOpen] = React.useState(true)
   const [tabIndex, setTabIndex] = React.useState(0)
+  const [uploadedVideos, setUploadedVideos] = React.useState<UserUploadedVideoStatisticsVO[]>([]);
+
+  React.useEffect(() => {
+    videoManagementAPI.getUploadedVideos().then(res => {
+      setUploadedVideos(res.data as UserUploadedVideoStatisticsVO[])
+    })
+  }, [])
+
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column' }}>
       <UploadDialog open={uploadDialogOpen} onClose={() => setUploadDialogOpen(false)} />
@@ -59,30 +72,37 @@ const UploadHome: React.FC = () => {
 
           </TableHead>
           <TableBody>
-            <TableRow>
-              <TableCell sx={{ paddingLeft: '32px', display: 'flex' }}>
-                {/* 视频封面 */}
-                <Box sx={{ position: 'relative', width: '127px', height: '72px', borderRadius: '8%', overflow: 'hidden', border: '1px #e3e3e3 solid' }}>
-                  <Box component='img' sx={{ width: '100%', height: '100%', positoin: 'absolute' }} src='/images/assert/login_bg.jpg' />
-                  <Box sx={{ borderRadius: '16%', position: 'absolute', bottom: 0, right: '4px', overflow: 'hidden' }}>
-                    <Typography sx={{ backgroundColor: 'rgba(0,0,0,0.6)', color: 'white', padding: '2px 4px' }} variant='caption'>12:34:56</Typography>
-                  </Box>
-                </Box>
-                {/* 视频标题与说明 */}
-                <Box sx={{ display: 'flex', flexDirection: 'column', marginLeft: '16px', justifyContent: 'space-evenly' }}>
-                  <Typography variant='body2'>视频标题</Typography>
-                  <Typography variant='body2'>点击添加说明</Typography>
-                </Box>
-              </TableCell>
-              <TableCell >公开范围</TableCell>
-              <TableCell>日期</TableCell>
-              <TableCell >观看次数</TableCell>
-              <TableCell >评论数</TableCell>
-              <TableCell >弹幕数</TableCell>
-              <TableCell >硬币数</TableCell>
-              <TableCell >收藏量</TableCell>
-              <TableCell >赞与不喜欢的比例</TableCell>
-            </TableRow>
+            {
+              uploadedVideos.map(val => {
+                return (
+                  <TableRow >
+                    <TableCell sx={{ paddingLeft: '32px', display: 'flex' }}>
+                      {/* 视频封面 */}
+                      <Box component='a' href={`/video/${val.nkid}`} target='_blank' sx={{ position: 'relative', width: '127px', height: '72px', borderRadius: '8%', overflow: 'hidden', border: '1px #e3e3e3 solid' }}>
+                        <Box component='img' sx={{ width: '100%', height: '100%', positoin: 'absolute' }} src={val.coverUrl} />
+                        <Box sx={{ borderRadius: '16%', position: 'absolute', bottom: 0, right: '4px', overflow: 'hidden' }}>
+                          <Typography sx={{ backgroundColor: 'rgba(0,0,0,0.6)', color: 'white', padding: '2px 4px' }} variant='caption'>12:34:56</Typography>
+                        </Box>
+                      </Box>
+                      {/* 视频标题与说明 */}
+                      <Box sx={{ display: 'flex', flexDirection: 'column', marginLeft: '16px', justifyContent: 'space-evenly' }}>
+                        <Typography variant='body2'>{val.title}</Typography>
+                        <Typography variant='body2' color='#a0a0a0'>{val.description}</Typography>
+                      </Box>
+                    </TableCell>
+                    <TableCell >公开范围</TableCell>
+                    <TableCell>{new Date(val.createdAt as Date).toLocaleDateString()}</TableCell>
+                    <TableCell >观看次数</TableCell>
+                    <TableCell >评论数</TableCell>
+                    <TableCell >弹幕数</TableCell>
+                    <TableCell >硬币数</TableCell>
+                    <TableCell >收藏量</TableCell>
+                    <TableCell >赞与不喜欢的比例</TableCell>
+                  </TableRow>
+                )
+              })
+            }
+
           </TableBody>
         </Table>
       </TableContainer>
