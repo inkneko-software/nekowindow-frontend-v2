b55355e5b90c155d8be8f84ee6c3a3c41f91aa2e
leaf-lxh <meleaf@qq.com>
2024-11-11 23:50:18 +0800

    refactor: 实现服务端渲染获取用户信息

--- src/app/account/home/page.tsx
+++ src/app/account/home/page.tsx
@@ -1,4 +1,3 @@
-'use client'
 import * as React from 'react'
 import Box from '@mui/material/Box';
 
@@ -6,49 +5,44 @@ import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import { Button, Checkbox, Chip, Divider, FormControlLabel } from '@mui/material';
 import Link from 'next/link';
+import { Configuration, UserControllerApi } from '@api/codegen/user';
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation';
 
 
-export default function AccountHome() {
-  const [info, setInfo] = React.useState({ uid: 0, face_url: "", nick: "", exp: 0 })
-  // React.useEffect(() => {
-  //     member.GetUser(null)
-  //         .then(res => res.data)
-  //         .then(res => {
-  //             if (res.code !== 0) {
-  //                 window.location.href = "/"
-  //             }
+export default async function AccountHome() {
+  var clientCookies = cookies()
+  console.log(clientCookies.toString());
+  const userAPI = new UserControllerApi(new Configuration({ credentials: 'include', basePath: process.env.NEXT_PUBLIC_API_SERVER, headers:{'Cookie': clientCookies.toString() } }));
 
-  //             var getuserdto: member.GetUserDTO = res.data
-  //             setInfo({
-  //                 uid: getuserdto.uid,
-  //                 face_url: getuserdto.face_url,
-  //                 nick: getuserdto.nick,
-  //                 exp: getuserdto.exp
-  //             })
-  //         })
-  // }, [])
+  const myUserDetailResponse = await userAPI.myUserDetail();
+  if (myUserDetailResponse.code !== 0 || myUserDetailResponse.data === undefined){
+    redirect('/error?reason=please_login_first')
+  }
+
+  const myUserDetail = myUserDetailResponse.data
 
   var expColor = "#ff905a"
   var currentLevel = "LV 1"
   var currentLevelExp = 1000;
-  var currentLevelProgress = (info.exp / currentLevelExp) * 100
+  var currentLevelProgress = (myUserDetail.exp / currentLevelExp) * 100
 
   return (
     <Box>
       <Box sx={{ display: "flex", flexDirection: "row" }}>
-        <Avatar sx={{ width: "64px", height: "64px" }} src={info.face_url} />
+        <Avatar sx={{ width: "64px", height: "64px" }} src={myUserDetail.avatarUrl} />
         <Box sx={{ display: "flex", flexDirection: "column", marginLeft: 2, flexGrow: 1 }}>
-          <Typography variant="h6">{info.nick}</Typography>
+          <Typography variant="h6">{myUserDetail.username}</Typography>
           <Box sx={{ height: "25px", display: "flex", marginTop: 1 }}>
             <Box sx={{ display: "flex", flexGrow: 1 }}>
               <Box sx={{ height: "100%", width: "50px", borderRadius: "4px 8px 8px 4px", backgroundColor: expColor, textAlign: 'center', color: "white" }} > {currentLevel}</Box>
               <Box sx={{ height: "100%", flexGrow: 1, borderRadius: "8px 4px 4px 8px ", background: `linear-gradient(90deg, ${expColor} ,${currentLevelProgress}%, white ${currentLevelProgress}% )`, border: `1px solid ${expColor}` }} />
             </Box>
-            <Box sx={{ margin: "0px 16px" }}>{info.exp} / {currentLevelExp}</Box>
+            <Box sx={{ margin: "0px 16px" }}>{myUserDetail.exp} / {currentLevelExp}</Box>
             <Link href="/account/myinfo" passHref>
               <Button sx={{ marginRight: 1, marginLeft: 5 }} variant="contained">修改资料</Button>
             </Link>
-            <Link href={`/space/${info.uid}`} passHref>
+            <Link href={`/space/${myUserDetail.uid}`} passHref>
               <Button variant="outlined" >个人空间</Button>
             </Link>
           </Box>
