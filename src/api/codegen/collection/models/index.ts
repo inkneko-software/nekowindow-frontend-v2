d42f4dcef47e562e82fc12f2518c443a0d8d1173
leaf-lxh <meleaf@qq.com>
2026-01-10 16:35:49 +0800

    feat: 收藏API

---
+++ src/api/codegen/collection/models/index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CollectionGroupVO';
+export * from './CollectionVideoPostVO';
+export * from './ResponseBoolean';
+export * from './ResponseCollectionGroupVO';
+export * from './ResponseListCollectionGroupVO';
+export * from './ResponseListCollectionVideoPostVO';
+export * from './ResponseObject';
