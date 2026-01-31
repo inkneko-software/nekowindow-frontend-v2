58ebc96c0210da12be4373523bf5b362c2f03d98
leaf-lxh <meleaf@qq.com>
2026-02-01 00:07:55 +0800

    fix: 修复数据重复

--- src/components/Recommend/RecommendPanel.tsx
+++ src/components/Recommend/RecommendPanel.tsx
@@ -1,12 +1,15 @@
+'use client'
 import { Box, Stack, Typography } from "@mui/material";
 import React from "react";
 import VideoPostBriefListItem from "../Media/VideoPostBriefListItem";
+import { Configuration, HomeRecommendVO, VideoControllerApi, VideoPostBriefVO } from "@api/codegen/video";
 
 interface RecommendPannelProps {
     sx?: any
 }
 function RecommendPanel(props: RecommendPannelProps) {
-    const {sx, ...others} = props
+    const videoAPI = new VideoControllerApi(new Configuration({ credentials: "include", basePath: process.env.NEXT_PUBLIC_API_SERVER }))
+    const { sx, ...others } = props
     var postInfo = {
         nkid: 3,
         archive_url: "/home_banner01.jpg",
@@ -15,17 +18,39 @@ function RecommendPanel(props: RecommendPannelProps) {
         danmaku: 0,
         duration: 0,
         created_at: new Date().getUTCMilliseconds(),
-        uploader_name:"LegendLXH",
+        uploader_name: "LegendLXH",
         uploader_id: 1
     }
     var data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] // video info json
 
+    var [recommend, setRecommend] = React.useState<VideoPostBriefVO[]>([])
+    React.useEffect(() => {
+        (async () => {
+            var homeRecommendRes = await videoAPI.getHomeRecommend();
+            var homeRecommend = homeRecommendRes.data as HomeRecommendVO;
+            var allVids = []
+            for (let i = 0; i < homeRecommend.partitionVideos.length; ++i){
+                allVids.push(...homeRecommend.partitionVideos[i])
+            }
+            setRecommend(allVids)
+        })()
+    }, [])
     return (
-        <Box sx={{...sx}}>
-            <Typography variant="h6" sx={{lineHeight:"40px"}}>视频推荐</Typography>
+        <Box sx={{ ...sx }}>
+            <Typography variant="h6" sx={{ lineHeight: "40px" }}>视频推荐</Typography>
             <Stack sx={{ ustifyContent: "right", marginLeft: 'auto', }} >
-                {data.map((val, index) => {
-                    return <VideoPostBriefListItem key={index} post={postInfo} sx={{}} />
+                {recommend.map((post, index) => {
+                    return <VideoPostBriefListItem key={index} post={{
+                        nkid: post.nkid,
+                        archive_url: post.coverUrl,
+                        title: post.title,
+                        visit: post.visit,
+                        danmaku: 0,
+                        duration: post.duration,
+                        created_at: new Date(post.createdAt).getTime(),
+                        uploader_name: post.uploader.username,
+                        uploader_id: post.uploader.userId
+                    }} sx={{}} />
                 })}
             </Stack>
         </Box>
